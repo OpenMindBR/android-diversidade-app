3298269fafd9d82795fbe639885deaeb2902ec43
Joamila Brito <joamila.brito@gmail.com>
2016-03-09 17:51:24 -0300

    -Menu: adição de seções, títulos, ícones e esquema de cores do app.

---
+++ app/src/main/java/br/edu/ifce/engcomp/francis/diversidade/Fragments/DiscoveringFragment.java
@@ -0,0 +1,30 @@
+package br.edu.ifce.engcomp.francis.diversidade.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.edu.ifce.engcomp.francis.diversidade.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DiscoveringFragment extends Fragment {
+
+
+    public DiscoveringFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discovering, container, false);
+    }
+
+}
--- app/src/main/java/br/edu/ifce/engcomp/francis/diversidade/MainActivity.java
+++ app/src/main/java/br/edu/ifce/engcomp/francis/diversidade/MainActivity.java
@@ -2,9 +2,8 @@ package br.edu.ifce.engcomp.francis.diversidade;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_nucleus) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_discovery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_rights) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_health) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_sugestion) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
